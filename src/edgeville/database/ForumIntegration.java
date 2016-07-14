b17716950d9969dbdbf4acdd751f16ff7612aebb
SJ <siminjacobs@gmail.com>
2016-07-14 09:06:31 +0200

    Fix connection by adding user agent

--- src/edgeville/database/ForumIntegration.java
+++ src/edgeville/database/ForumIntegration.java
@@ -82,6 +82,7 @@ public class ForumIntegration {
 
 			URL url = new URL(urlString);
 			URLConnection urlConnection = url.openConnection();
+			urlConnection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
 			//((HttpURLConnection) urlConnection).setRequestMethod("POST");
 			//urlConnection.setDoOutput(true);
 			InputStream is = urlConnection.getInputStream();
