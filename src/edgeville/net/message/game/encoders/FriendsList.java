6b0d265371f2de9abbbeadb503c8fb4b3ed341d7
SJacobs <siminjacobs@gmail.com>
2016-07-28 17:46:54 +0200

    backup before messing with cache

--- src/edgeville/net/message/game/encoders/FriendsList.java
+++ src/edgeville/net/message/game/encoders/FriendsList.java
@@ -1,5 +1,8 @@
 package edgeville.net.message.game.encoders;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edgeville.io.RSBuffer;
 import edgeville.model.GroundItem;
 import edgeville.model.entity.Player;
@@ -10,26 +13,37 @@ import io.netty.buffer.Unpooled;
  */
 public class FriendsList implements Command {
 
-	
 	public FriendsList() {
 	}
 
+	//58 to set friends list to connect
+
 	@Override
 	public RSBuffer encode(Player player) {
-		RSBuffer buffer = new RSBuffer(Unpooled.buffer());
 
-		buffer.packet(195);
-		
+		String guy1 = "Guy1";
+		String guy2 = "Guy2";
+		String guy3 = "Guy3";
+
+		String guy4 = "Guy4";
+		String guy5 = "Guy5";
+		String guy6 = "Guy6";
+
+		int size = guy1.getBytes().length + guy2.getBytes().length + guy3.getBytes().length;
+		size = size + 1 + 2 + 1 + 1;
+
+		RSBuffer buffer = new RSBuffer(player.channel().alloc().buffer(size));
+		buffer.packet(195).writeSize(RSBuffer.SizeType.SHORT);
+
 		buffer.writeByte(1);
-		buffer.writeString("Test1");
-		buffer.writeString("Test2");
+		buffer.writeString(guy1);
+		buffer.writeString(guy2);
 		buffer.writeShort(0);
 		buffer.writeByte(1);
 		buffer.writeByte(1);
-		
-		buffer.writeString("Test3");
+		buffer.writeString(guy3);
+
 		return buffer;
 	}
 
 }
-
